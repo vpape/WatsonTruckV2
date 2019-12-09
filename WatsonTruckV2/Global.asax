cf67c9099ffcdb3d958f9d85db0496eb658183ac
vpape <v.pape3@gmail.com>
2019-12-09 17:12:59 -0600

    Add project files.

---
+++ WatsonTruckV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WatsonTruckV2.MvcApplication" Language="C#" %>
---
+++ WatsonTruckV2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WatsonTruckV2.Startup))]
+namespace WatsonTruckV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
